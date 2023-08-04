95bf179ece7c44abbaaa0167d9bbc5b45ad91395
Pe4enkaRU <137439630+Pe4enkaRU@users.noreply.github.com>
2023-08-04 17:10:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let xaxa = document.querySelectorAll('.cell');
+let suka = document.querySelectorAll('#dada');
+
+let player = "x";
+
+
+
+let dav = document.createElement('div');
+dav.className = "cell";
+dav.innerHTML = "x";
+
+suka.prepend(dav)
+
+for (let i = 0; i < xaxa.length; i++) {
+	xaxa[i].onclick = function(){
+
+}
+}
+
